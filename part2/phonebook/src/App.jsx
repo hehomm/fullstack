bef0dc73937152597f3c5215ab3f161c3af81555
Henrietta Homm <henriettahomm@gmail.com>
2024-01-27 18:34:29 +0200

    Exercise 2.9.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,41 @@
 import { useState } from 'react'
 
+const RenderNames = ({persons}) => {
+  return (
+    <div>
+      {persons.map(person => <div key={person.name} >{person.name} {person.number}</div>)}
+    </div>
+    )
+}
+
+const Display = ({ persons, search }) => {
+  // display all names if search bar is empty
+  // otherwise display search results
+  if (search==='') return <><RenderNames persons={persons}/></>
+  const re = new RegExp(`${search}`, 'i')
+  console.log('re', re);
+  const found = persons.filter(person => re.test(person.name))
+  console.log(found);
+  return <><RenderNames persons={found}/></>
+}
+
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault()
     console.log(newName);
+    // check if name is already in persons
     const exists = persons.reduce((prev, current) => prev ? true : current.name===newName, false)
     console.log(exists)
     if (exists) {
       alert(`${newName} is already added to the phonebook`)
       return
     }
+    // don't allow empty name submissions
     if (newName==='') {
       alert('Enter a name')
       return
@@ -37,9 +59,21 @@ const App = () => {
     setNewNumber(num)
   }
 
+  const handleSearch = (event) => {
+    console.log(event.target.value)
+    setSearch(event.target.value)
+  }
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <form>
+        <div>
+          search: <input value={search} onChange={handleSearch}/>
+        </div>
+      </form>
+
+      <h2>Add new name</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -52,7 +86,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name} >{person.name} {person.number}</div>)}
+      <Display persons={persons} search={search}/>
     </div>
   )
 }
